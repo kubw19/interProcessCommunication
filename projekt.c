7d43c045ff2e64541b26ae2a6d0beec8d102f475
Jakub Włodarczyk <kubw19@gmail.com>
2018-12-31 12:33:19 +0100

    Cleaned code, data transfer between three child processes. First child transmits size of string and string to second child via fifo pipe, and them second child transmits the same data to child 3 via unnamed pipe.

--- projekt.c
+++ projekt.c
@@ -6,28 +6,21 @@
 #include <stdbool.h>
 #include <sys/stat.h>
 
-int deskryptory[2];
-
-int des, des2; 
 pid_t d1, d2, d3, rodzic;
-bool pobierzCiag, wczytajPlik;
 int deskryptory[2];
 
 
 void posrednik1(int signal){
-	//printf("posrednik\n");	
 	kill(d2, 10);
 	return;
 }
 
 void posrednik2(int signal){
-	//printf("posrednik2\n");	
 	kill(d1, 10);
 	return;
 }
 
 void dziecko1(){
-	//if(pobierzCiag==1){
 	int i = 0;	
 	char *znaki = malloc(sizeof(char));
 	read(0, &znaki[i], 1);
@@ -36,74 +29,42 @@ void dziecko1(){
 		znaki = realloc(znaki, sizeof(char)*(i+1));
 		read(0, &znaki[i], 1);
 	}
+	znaki[i]='\0';
 	des = open( "/tmp/plikfifo", O_WRONLY);
-	i = 0;
-	while(znaki[i]!='\n'){
-		write(des, &znaki[i], 1);
-		i++;
-	}
-	znaki[i+1]='\0';
-    write(des, &znaki[i+1], 1);
-	//printf("zapisalem\n");
-
-	//write(des, &val, 1);
-	//kill(rodzic,10);						
+	write(des, &i, sizeof(int));
+    write(des, znaki, i*sizeof(char));			
 	close(des);
-	//pobierzCiag=false;
-//}	
 }
 
 void dziecko2(){
-	int des;
-	//des = open( "/tmp/plik.txt", O_CREAT | O_RDWR| O_APPEND,  0666 );
-	//des2 = open( "/tmp/plik", O_WRONLY);		
-	//sleep(4);
-	des = open( "/tmp/plikfifo", O_RDONLY);
-	int i = 0;
-	char znak;
-	read(des, &znak, 1);
-	printf("%c", znak);
-	while(znak!='\0'){
-		read(des, &znak, 1);
-		printf("%c", znak);
-	}
-	//printf("\n");
-	fflush(stdout);
-	printf("\n");
-	//printf("z dziecka2: %s\n",znak);
+	int des = open( "/tmp/plikfifo", O_RDONLY);
+	int size;
+	read(des, &size, sizeof(int));
+	char *znaki = malloc(sizeof(char)*size);
+	read(des, znaki, size);
+	znaki[size]='\0';
 	close(deskryptory[0]);
-//	write(deskryptory[1], &size,1);
-	//printf("%s\n", znak);
-	//write(deskryptory[1], znak,size);
-	//close(deskryptory[1]);
-	//write(des2,&znak, 1);
-	//close(des2);
-	//close(des);
+	write(deskryptory[1], &size,sizeof(int));
+	write(deskryptory[1], znaki,size*sizeof(char));
 }
 
 void dziecko3(){
-	//int des;
-	//des = open( deskryptory[0], O_RDONLY);
 	close(deskryptory[1]);
 	int size;
-	read(deskryptory[0], &size, 1);
-	//size++;
-	char *znak = malloc(size*sizeof(char));
-	read(deskryptory[0], znak,size);
-	//close(deskryptory[1]);
-	//printf("%s\n", znak);
-	//write(des2,&znak, 1);
-	//close(des2);
+	read(deskryptory[0], &size, sizeof(int));
+	char *znaki = malloc(sizeof(char)*size);
+	read(deskryptory[0], znaki,size);
+	znaki[size]='\0';
 
-
+	printf("dziecko3: %s\n", znaki);
 }
 
 
 int main(){
 	mknod("/tmp/plikfifo", S_IFIFO|0666, 0);
 	pipe(deskryptory);
-	pobierzCiag = true;	
 	rodzic = getpid();
+	
 	if((d1=fork())){
 		if((d2=fork())){
 			if((d3=fork())){
@@ -119,25 +80,15 @@ int main(){
 		}
 		else{	
 				//2 dziecko
-				//signal(10, saveSignal);
-					//printf("wczytuje\n");
-				//kill(rodzic, 12);
 				while(1){
 					dziecko2();
 				};
-				
-				
-				
-
 		}
 	}
 	else{
 		//1dziecko
-			//signal(10, readSignal);
 			while(1){
 				dziecko1();
 			}
-
-		while(1);
 	}
 }
